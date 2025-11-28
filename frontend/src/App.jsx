3dd7782e39762896254504f4854d70f4017a3301
tan nguyen <tannpv@gmail.com>
2025-11-28 17:21:12 +0700

    feat: add office pipeline frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import FieldToday from "./pages/FieldToday.jsx";
 import FieldCard from "./pages/FieldCard.jsx";
+import OfficePipeline from "./pages/OfficePipeline.jsx";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Route path="/" element={<FieldToday />} />
       <Route path="/field/today" element={<FieldToday />} />
       <Route path="/field/card/:id" element={<FieldCard />} />
+      <Route path="/office/pipeline" element={<OfficePipeline />} />
     </Routes>
   );
 }
